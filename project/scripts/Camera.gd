da2c9d8f581038329abd35768381470348f48b79
kjniemela <johannes@niemela.se>
2024-01-28 15:08:50 -0600

    disable camera smoothing until we can solver the jitter issue

--- project/scripts/Camera.gd
+++ project/scripts/Camera.gd
@@ -4,6 +4,7 @@ extends Camera2D
 @export var cam_speed = 500
 
 
-func _process(delta):
-    var diff = %Player.position - position
-    position += (diff * 0.1).limit_length(cam_speed * delta)
+func _physics_process(delta):
+    #var diff = %Player.position - position
+    #position += (diff * 0.1).limit_length(cam_speed * delta)
+    position = %Player.position
